15a02478ac9c0d8b20e9b2881a10b4daac9cca49
kophy <kophy@protonmail.com>
2019-02-02 14:51:31 -0800

    2019.02.02

--- 03/3.27.rkt
+++ 03/3.27.rkt
@@ -2,29 +2,7 @@
 
 (require racket/mpair)
 (require rackunit)
-
-(define (lookup key table)
-  (let ((record (assoc key (mcdr table))))
-    (if record
-        (mcdr record)
-        #f)))
-
-(define (assoc key records)
-  (cond ((null? records) #f)
-        ((equal? key (mcar (mcar records))) (mcar records))
-        (else (assoc key (mcdr records)))))
-
-(define (insert! key value table)
-  (let ((record (assoc key (mcdr table))))
-    (if record
-        (set-mcdr! record value)
-        (set-mcdr! table
-                  (mcons (mcons key value)
-                         (mcdr table)))))
-  'ok)
-
-(define (make-table)
-  (mlist '*table*))
+(require "lib/table.rkt")
 
 (define (memoize f)
   (let ((table (make-table)))
